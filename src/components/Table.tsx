f3a97febb3d92ceba60b061bfe5fbe41289ccff3
jordan <sudo.sendmail.jordan@gmail.com>
2024-03-10 00:37:04 -0800

    Update Table.tsx

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,16 +1,24 @@
 import "./Table.css";
 import { Link } from "react-router-dom";
 
+import { FaRegCopy } from "react-icons/fa";
+import { RiPlayListAddLine } from "react-icons/ri";
 function TableCard() {
   return (
     <>
       <section className="table-card">
-        <div>package_name</div>
-        <div>maintainer</div>
+        <div>
+          package_name
+          <RiPlayListAddLine />
+        </div>
         <Link style={{ border: "none" }} to="/package">
-          description
+          maintainer
         </Link>
-        <div>copy_command</div>
+        <em>description</em>
+        <div>
+          <FaRegCopy />
+          copy_command
+        </div>
       </section>
     </>
   );
