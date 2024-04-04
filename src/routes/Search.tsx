297a68eeac92ab59a5b986cb464793ef8ff11976
jordan <sudo.sendmail.jordan@gmail.com>
2024-04-04 15:10:39 -0700

    Update Search.tsx

--- src/routes/Search.tsx
+++ src/routes/Search.tsx
@@ -5,14 +5,6 @@ export default function Search() {
   return (
     <>
       <Table />
-      <section className="main-container">
-        <p> 🔨 In Developement </p>
-        <div>search_page</div>
-
-        <p>
-          🔨 Contribute <a>https://github.com/apt-run </a> !
-        </p>
-      </section>
     </>
   )
 }
